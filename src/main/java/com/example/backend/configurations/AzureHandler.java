f293eb45038e8e698803ef3a403f4f99e47b0cd7
Anh Tang <anh.tang@opswat.com>
2024-08-01 18:43:31 +0700

    update

--- src/main/java/com/example/backend/configurations/AzureHandler.java
+++ src/main/java/com/example/backend/configurations/AzureHandler.java
@@ -1,5 +1,6 @@
 package com.example.backend.configurations;
 
+import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
@@ -19,7 +20,8 @@ public class AzureHandler {
             @HttpTrigger(name = "request", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<User>> request, ExecutionContext context) {
         User user = request.getBody()
                 .filter(u -> u.getDisplayName() != null)
-                .orElseGet(() -> new User());
+                .orElseGet(User::new);
+        context.getLogger().info("Greeting user name: " + user.getDisplayName());
         return request.createResponseBuilder(HttpStatus.OK)
                 .body("Hello")
                 .header("Content-Type", "application/json")
