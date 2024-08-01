33c029884873b9ecd30aaa57756703edf06ecf87
Anh Tang <anh.tang@opswat.com>
2024-08-01 15:06:49 +0700

    update

--- src/main/java/com/example/backend/configurations/AzureConfiguration.java
+++ src/main/java/com/example/backend/configurations/AzureConfiguration.java
@@ -4,10 +4,21 @@ import com.azure.core.credential.TokenCredential;
 import com.azure.identity.ClientSecretCredentialBuilder;
 import com.azure.spring.cloud.autoconfigure.implementation.context.AzureTokenCredentialAutoConfiguration;
 import com.example.backend.shared.Constant;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.OutputBinding;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.azure.functions.annotation.QueueOutput;
 import com.microsoft.graph.core.authentication.AzureIdentityAuthenticationProvider;
 import com.microsoft.graph.serviceclient.GraphServiceClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
 
 @Configuration
 public class AzureConfiguration {
@@ -30,4 +41,13 @@ public class AzureConfiguration {
                 .build();
     }
 
+    @FunctionName("benna")
+    public HttpResponseMessage run(
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS)
+            HttpRequestMessage<Optional<String>> request,
+            @QueueOutput(name = "msg", queueName = "outqueue",
+                    connection = "AzureWebJobsStorage") OutputBinding<String> msg,
+            final ExecutionContext context){
+        return null;
+    }
 }
